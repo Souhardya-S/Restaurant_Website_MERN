e6bfb917907c5b9932f4c185acbe43929ec4bb18
Souhardya-S <souhardyas2003@gmail.com>
2024-07-16 14:24:57 +0530

    first commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">SOUHARDYA</div>
+          <div className="right">
+            <p>Barasat,Kolkata-700124</p>
+            <p>Open: 4:00pm - 11:00pm</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Me</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Souhardya</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
